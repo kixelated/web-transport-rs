e094681229a77210628108aae99d27138cb3a492
Luke Curley <kixelated@gmail.com>
2023-07-19 23:39:32 -0700

    Accept streams in PARALLEL.
    The old code suffered from head-of-line blocking, as it would block
    reading the stream header in accepted order. This code will accept and
    decode streams in parallel, allowing us to return the first ready stream
    even if it has a higher stream ID (invisible to WebTransport).

--- webtransport-quinn/src/session.rs
+++ webtransport-quinn/src/session.rs
@@ -1,11 +1,12 @@
 use std::{
-    ops::{Deref, DerefMut},
-    pin::pin,
+    future::{poll_fn, Future},
+    ops::Deref,
+    pin::{pin, Pin},
     sync::{Arc, Mutex},
-    task::{Context, Poll},
+    task::{ready, Context, Poll},
 };
 
-use futures::Future;
+use futures::stream::{FuturesUnordered, Stream, StreamExt};
 
 use crate::{Connect, RecvStream, SendStream, SessionError, Settings, WebTransportError};
 
@@ -18,25 +19,14 @@ use webtransport_proto::{Frame, StreamUni, VarInt};
 ///   2. Errors codes are encoded with the session ID, so they aren't full QUIC error codes.
 ///   3. Stream IDs may have gaps in them, used by HTTP/3 transparant to the application.
 ///
-/// Any non-overloaded methods can be using `Deref`. Be careful if you suspect a new Quinn method is not compatible with WebTransport and needs to be wrapped.
-///
-/// The session can be cloned so it can be accessed from multiple handles.
+/// Deref is used to expose non-overloaded methods on [`quinn::Connection`].
+/// These should be safe to use with WebTransport, but file a PR if you find one that isn't.
 #[derive(Clone)]
 pub struct Session {
     conn: quinn::Connection,
-    session_id: VarInt,
 
-    // Keep a reference to the settings and connect stream to avoid closing them.
-    // We use Arc so the session can be cloned.
-    #[allow(dead_code)]
-    settings: Arc<Settings>,
-    #[allow(dead_code)]
-    connect: Arc<Connect>,
-
-    // We also need to keep a reference to the qpack streams if the endpoint (incorrectly) creates them.
-    // Again, this is just so they don't get closed until we drop the session.
-    qpack_encoder: Arc<Mutex<Option<quinn::RecvStream>>>,
-    qpack_decoder: Arc<Mutex<Option<quinn::RecvStream>>>,
+    // The accept logic is stateful, so use an Arc<Mutex> to share it.
+    accept: Arc<Mutex<SessionAccept>>,
 
     // Cache the headers in front of each stream we open.
     header_uni: Vec<u8>,
@@ -57,18 +47,25 @@ impl Session {
         Frame::WEBTRANSPORT.encode(&mut header_bi);
         session_id.encode(&mut header_bi);
 
+        // Accept logic is stateful, so use an Arc<Mutex> to share it.
+        let accept = SessionAccept::new(conn.clone(), settings, connect);
+
         Self {
             conn,
-
-            settings: Arc::new(settings),
-            connect: Arc::new(connect),
-            qpack_decoder: Arc::new(Mutex::new(None)),
-            qpack_encoder: Arc::new(Mutex::new(None)),
-
-            session_id,
+            accept: Arc::new(Mutex::new(accept)),
             header_uni,
             header_bi,
         }
+    }
+
+    /// Accept a new unidirectional stream. See [`quinn::Connection::accept_uni`].
+    pub async fn accept_uni(&self) -> Result<RecvStream, SessionError> {
+        poll_fn(|cx| self.accept.lock().unwrap().poll_accept_uni(cx)).await
+    }
+
+    /// Accept a new bidirectional stream. See [`quinn::Connection::accept_bi`].
+    pub async fn accept_bi(&self) -> Result<(SendStream, RecvStream), SessionError> {
+        poll_fn(|cx| self.accept.lock().unwrap().poll_accept_bi(cx)).await
     }
 
     /// Open a new unidirectional stream. See [`quinn::Connection::open_uni`].
@@ -83,72 +80,6 @@ impl Session {
         let (mut send, recv) = self.conn.open_bi().await?;
         Self::write_full(&mut send, &self.header_bi).await?;
         Ok((SendStream::new(send), RecvStream::new(recv)))
-    }
-
-    /// Accept a new unidirectional stream. See [`quinn::Connection::accept_uni`].
-    pub async fn accept_uni(&self) -> Result<RecvStream, SessionError> {
-        loop {
-            let mut recv = self.conn.accept_uni().await?;
-
-            let typ = Self::read_varint(&mut recv).await?;
-            let typ = StreamUni(typ);
-            if typ.is_reserved() {
-                // HTTP/3 reserved streams are ignored.
-                continue;
-            }
-
-            match typ {
-                StreamUni::WEBTRANSPORT => {
-                    // Read the session ID and make sure it matches.
-                    let session_id = Self::read_varint(&mut recv).await?;
-                    if session_id != self.session_id {
-                        return Err(WebTransportError::UnknownSession.into());
-                    }
-
-                    // Wrap the stream so we'll correct the error types.
-                    let recv = RecvStream::new(recv);
-
-                    return Ok(recv);
-                }
-                StreamUni::QPACK_ENCODER => {
-                    // Save the qpack encoder stream so we will close it on drop.
-                    // It's technically an error to send two of these but who cares, we'll close the previous one.
-                    let mut qpack_encoder = self.qpack_encoder.lock().unwrap();
-                    *qpack_encoder = Some(recv);
-                }
-                StreamUni::QPACK_DECODER => {
-                    // Save the qpack encoder stream so we will close it on drop.
-                    // It's technically an error to send two of these but who cares, we'll close the previous one.
-                    let mut qpack_decoder = self.qpack_decoder.lock().unwrap();
-                    *qpack_decoder = Some(recv);
-                }
-
-                // Who knows what this stream is for.
-                // Close it, try another, and hope the other endpoint doesn't close the session.
-                _ => (),
-            }
-        }
-    }
-
-    /// Accept a new bidirectional stream. See [`quinn::Connection::accept_bi`].
-    pub async fn accept_bi(&self) -> Result<(SendStream, RecvStream), SessionError> {
-        loop {
-            let (send, mut recv) = self.conn.accept_bi().await?;
-
-            let typ = Self::read_varint(&mut recv).await?;
-            if Frame(typ) != Frame::WEBTRANSPORT {
-                // Close it, accept the next, and hope we don't error.
-                // TODO write a Method not Allowed HTTP/3 response?
-                continue;
-            }
-
-            let session_id = Self::read_varint(&mut recv).await?;
-            if session_id != self.session_id {
-                return Err(WebTransportError::UnknownSession.into());
-            }
-
-            return Ok((SendStream::new(send), RecvStream::new(recv)));
-        }
     }
 
     pub async fn read_datagram(&self) {
@@ -179,6 +110,193 @@ impl Session {
         self.conn.close_reason().map(Into::into)
     }
 
+    async fn write_full(send: &mut quinn::SendStream, buf: &[u8]) -> Result<(), SessionError> {
+        match send.write_all(buf).await {
+            Ok(_) => Ok(()),
+            Err(quinn::WriteError::ConnectionLost(err)) => Err(err.into()),
+            Err(err) => Err(WebTransportError::WriteError(err).into()),
+        }
+    }
+}
+
+impl Deref for Session {
+    type Target = quinn::Connection;
+
+    fn deref(&self) -> &Self::Target {
+        &self.conn
+    }
+}
+
+// Type aliases just so clippy doesn't complain about the complexity.
+type AcceptUni = dyn Stream<Item = Result<quinn::RecvStream, quinn::ConnectionError>> + Send;
+type AcceptBi = dyn Stream<Item = Result<(quinn::SendStream, quinn::RecvStream), quinn::ConnectionError>>
+    + Send;
+type PendingUni = dyn Future<Output = Result<(StreamUni, quinn::RecvStream), SessionError>> + Send;
+type PendingBi = dyn Future<Output = Result<Option<(SendStream, RecvStream)>, SessionError>> + Send;
+
+// Logic just for accepting streams, which is annoying because of the stream header.
+pub struct SessionAccept {
+    session_id: VarInt,
+
+    // Keep a reference to the settings and connect stream to avoid closing them until dropped.
+    #[allow(dead_code)]
+    settings: Settings,
+    #[allow(dead_code)]
+    connect: Connect,
+
+    // We also need to keep a reference to the qpack streams if the endpoint (incorrectly) creates them.
+    // Again, this is just so they don't get closed until we drop the session.
+    qpack_encoder: Option<quinn::RecvStream>,
+    qpack_decoder: Option<quinn::RecvStream>,
+
+    accept_uni: Pin<Box<AcceptUni>>,
+    accept_bi: Pin<Box<AcceptBi>>,
+
+    // Keep track of work being done to read/write the WebTransport stream header.
+    pending_uni: FuturesUnordered<Pin<Box<PendingUni>>>,
+    pending_bi: FuturesUnordered<Pin<Box<PendingBi>>>,
+}
+
+impl SessionAccept {
+    pub(crate) fn new(conn: quinn::Connection, settings: Settings, connect: Connect) -> Self {
+        // The session ID is the stream ID of the CONNECT request.
+        let session_id = connect.session_id();
+
+        // Create a stream that just outputs new streams, so it's easy to call from poll.
+        let accept_uni = Box::pin(futures::stream::unfold(conn.clone(), |conn| async {
+            Some((conn.accept_uni().await, conn))
+        }));
+
+        let accept_bi = Box::pin(futures::stream::unfold(conn, |conn| async {
+            Some((conn.accept_bi().await, conn))
+        }));
+
+        Self {
+            session_id,
+
+            settings,
+            connect,
+            qpack_decoder: None,
+            qpack_encoder: None,
+
+            accept_uni,
+            accept_bi,
+
+            pending_uni: FuturesUnordered::new(),
+            pending_bi: FuturesUnordered::new(),
+        }
+    }
+
+    // This is poll-based because we accept and decode streams in parallel.
+    // In async land I would use tokio::JoinSet, but that requires a runtime.
+    // It's better to use FuturesUnordered instead because it's agnostic.
+    pub fn poll_accept_uni(
+        &mut self,
+        cx: &mut Context<'_>,
+    ) -> Poll<Result<RecvStream, SessionError>> {
+        // Accept any new streams.
+        if let Poll::Ready(Some(res)) = self.accept_uni.poll_next_unpin(cx) {
+            // Start decoding the header and add the future to the list of pending streams.
+            let recv = res?;
+            let pending = Self::decode_uni(recv, self.session_id);
+            self.pending_uni.push(Box::pin(pending));
+        }
+
+        loop {
+            // Poll the list of pending streams.
+            let (typ, recv) = match ready!(self.pending_uni.poll_next_unpin(cx)) {
+                Some(res) => res?,
+                None => return Poll::Pending,
+            };
+
+            // Decide if we keep looping based on the type.
+            match typ {
+                StreamUni::WEBTRANSPORT => {
+                    let recv = RecvStream::new(recv);
+                    return Poll::Ready(Ok(recv));
+                }
+                StreamUni::QPACK_DECODER => {
+                    self.qpack_decoder = Some(recv);
+                }
+                StreamUni::QPACK_ENCODER => {
+                    self.qpack_encoder = Some(recv);
+                }
+                _ => {} // ignore unknown streams
+            }
+        }
+    }
+
+    // Reads the stream header, returning the stream type.
+    async fn decode_uni(
+        mut recv: quinn::RecvStream,
+        expected_session: VarInt,
+    ) -> Result<(StreamUni, quinn::RecvStream), SessionError> {
+        // Read the VarInt at the start of the stream.
+        let typ = Self::read_varint(&mut recv).await?;
+        let typ = StreamUni(typ);
+
+        if typ == StreamUni::WEBTRANSPORT {
+            // Read the session_id and validate it
+            let session_id = Self::read_varint(&mut recv).await?;
+            if session_id != expected_session {
+                return Err(WebTransportError::UnknownSession.into());
+            }
+        }
+
+        // We need to keep a reference to the qpack streams if the endpoint (incorrectly) creates them, so return everything.
+        Ok((typ, recv))
+    }
+
+    pub fn poll_accept_bi(
+        &mut self,
+        cx: &mut Context<'_>,
+    ) -> Poll<Result<(SendStream, RecvStream), SessionError>> {
+        // Accept any new streams.
+        if let Poll::Ready(Some(res)) = self.accept_bi.poll_next_unpin(cx) {
+            // Start decoding the header and add the future to the list of pending streams.
+            let (send, recv) = res?;
+            let pending = Self::decode_bi(send, recv, self.session_id);
+            self.pending_bi.push(Box::pin(pending));
+        }
+
+        loop {
+            // Poll the list of pending streams.
+            let res = match ready!(self.pending_bi.poll_next_unpin(cx)) {
+                Some(res) => res?,
+                None => return Poll::Pending,
+            };
+
+            if let Some((send, recv)) = res {
+                return Poll::Ready(Ok((send, recv)));
+            }
+
+            // Keep looping if it's a stream we want to ignore.
+        }
+    }
+
+    // Reads the stream header, returning Some if it's a WebTransport stream.
+    async fn decode_bi(
+        send: quinn::SendStream,
+        mut recv: quinn::RecvStream,
+        expected_session: VarInt,
+    ) -> Result<Option<(SendStream, RecvStream)>, SessionError> {
+        let typ = Self::read_varint(&mut recv).await?;
+        if Frame(typ) != Frame::WEBTRANSPORT {
+            return Ok(None);
+        }
+
+        // Read the session ID and validate it.
+        let session_id = Self::read_varint(&mut recv).await?;
+        if session_id != expected_session {
+            return Err(WebTransportError::UnknownSession.into());
+        }
+
+        // Wrap the streams in our own types for correct error codes.
+        let send = SendStream::new(send);
+        let recv = RecvStream::new(recv);
+
+        Ok(Some((send, recv)))
+    }
     // Read into the provided buffer and cast any errors to SessionError.
     async fn read_full(recv: &mut quinn::RecvStream, buf: &mut [u8]) -> Result<(), SessionError> {
         match recv.read_exact(buf).await {
@@ -208,28 +326,6 @@ impl Session {
 
         Ok(v)
     }
-
-    async fn write_full(send: &mut quinn::SendStream, buf: &[u8]) -> Result<(), SessionError> {
-        match send.write_all(buf).await {
-            Ok(_) => Ok(()),
-            Err(quinn::WriteError::ConnectionLost(err)) => Err(err.into()),
-            Err(err) => Err(WebTransportError::WriteError(err).into()),
-        }
-    }
-}
-
-impl Deref for Session {
-    type Target = quinn::Connection;
-
-    fn deref(&self) -> &Self::Target {
-        &self.conn
-    }
-}
-
-impl DerefMut for Session {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.conn
-    }
 }
 
 impl webtransport_generic::Session for Session {
@@ -244,7 +340,7 @@ impl webtransport_generic::Session for Session {
         &mut self,
         cx: &mut Context<'_>,
     ) -> Poll<Result<Self::RecvStream, Self::Error>> {
-        pin!(self.accept_uni()).poll(cx)
+        self.accept.lock().unwrap().poll_accept_uni(cx)
     }
 
     /// Accept an incoming bidirectional stream
@@ -254,7 +350,7 @@ impl webtransport_generic::Session for Session {
         &mut self,
         cx: &mut Context<'_>,
     ) -> Poll<Result<(Self::SendStream, Self::RecvStream), Self::Error>> {
-        pin!(self.accept_bi()).poll(cx)
+        self.accept.lock().unwrap().poll_accept_bi(cx)
     }
 
     /// Poll the connection to create a new bidirectional stream.
