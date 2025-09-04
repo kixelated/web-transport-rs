5d1bce2463e9ae385bcac086e3bfd53e4bf7045f
Luke Curley <kixelated@gmail.com>
2025-09-04 11:08:47 -0700

    Publish NPM package. (#96)
    * Publish JS
    
    * Maybe fix node imports?
    
    * Another attempt.
    
    * Include the licenses.
    
    * Missed one, but it works now.

---
+++ web-transport-ws/scripts/release.ts
@@ -0,0 +1,14 @@
+// ChatGPT made a script that rewrites package.json file to use the correct paths.
+// The problem is that I want paths to reference `src` during development, but `dist` during release.
+// It's not pretty but nothing in NPM is.
+
+import { execSync } from "node:child_process";
+
+console.log("📦 Building package...");
+execSync("pnpm build", { stdio: "inherit" });
+
+console.log("🚀 Publishing...");
+execSync("pnpm publish --access=public", {
+	stdio: "inherit",
+	cwd: "dist",
+});
--- web-transport-ws/src/index.ts
+++ web-transport-ws/src/index.ts
@@ -1,9 +1,8 @@
-import WebTransportWs from "./session";
+import WebTransportWs from "./session.ts";
 
 // Install polyfill if WebTransport is not available, returning true if installed
 export function install(): boolean {
     if ("WebTransport" in globalThis) return false;
-
     // biome-ignore lint/suspicious/noExplicitAny: polyfill
     (globalThis as any).WebTransport = WebTransportWs;
     return true
